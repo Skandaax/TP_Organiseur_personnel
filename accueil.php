b3a4cfaf827a41d7dffe43eedc2bd318442bfb4c
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-04-09 14:44:03 +0200

    modification fichier

---
+++ accueil.php
@@ -0,0 +1,73 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<div class="tab">
+<table id="table1">
+    <thead>
+        <tr>
+            <th> Inscription </th>
+        </tr>
+        
+        <tr>
+            <th><form action="#" method="POST"></th>
+        </tr>
+        <tr>
+            <th><label for="ide">Votre identifiant</label></th>
+        </tr>
+        <tr>
+            <th><input type="text" id="ide" name="ide" placeholder="Votre identifiant"></th>
+        </tr>
+        <tr>
+        <tr>
+            <th><label for="passw">Votre mot de passe</label>
+        </tr>
+        <tr>    
+            <th><input type="text" id="passw" name="passw" placeholder="Votre e:mail"><th>
+        </tr>
+        <tr>
+            <th><input type="submit" value="Envoyer"></th> 
+        </form>    
+        </tr>
+    </thead>
+</table>
+<table id="table2">
+    <thead>
+        <tr>
+            <th> Connexion </th>
+        </tr>
+        
+        <tr>
+            <th><form action="#" method="POST"></th>
+        </tr>
+        <tr>
+            <th><label for="id">Votre identifiants</label></th>
+        </tr>
+        <tr>
+            <th><input type="text" id="id" name="id" placeholder="Votre identifiant"></th>
+        </tr>
+            <th><label for="pass">Votre mot de passe</label>
+        </tr>
+        <tr>    
+            <th><input type="text" id="pass" name="pass" placeholder="Votre mot de passe"><th>
+        </tr>
+        <tr>
+            <th><input type="submit" value="Envoyer"></th> 
+        </form>    
+        </tr>
+</table>
+</div>
+
+</body>
+</html>
